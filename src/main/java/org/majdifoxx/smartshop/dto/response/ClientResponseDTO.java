d2187b02e898522cf47586d53564ef4e177d34af
majdifox <majmehdi20@gmail.com>
2025-11-26 00:50:45 +0100

    adding the ClientResponseDTO

--- src/main/java/org/majdifoxx/smartshop/dto/response/ClientResponseDTO.java
+++ src/main/java/org/majdifoxx/smartshop/dto/response/ClientResponseDTO.java
@@ -1,32 +1,32 @@
-package org.majdifoxx.smartshop.dto;
+package org.majdifoxx.smartshop.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.majdifoxx.smartshop.enums.CustomerTier;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-/**
- * Response DTO with full client details
- */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ClientResponseDTO {
 
-    private String id;
+    private Long id;
     private String name;
     private String email;
     private String phone;
     private String address;
+
     private CustomerTier tier;
     private Integer totalOrders;
     private BigDecimal totalSpent;
     private LocalDateTime firstOrderDate;
     private LocalDateTime lastOrderDate;
+
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
