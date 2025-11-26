5acf37629e2cf3de206f88c6dfa652cefa849c8e
majdifox <majmehdi20@gmail.com>
2025-11-26 10:57:12 +0100

    adding the OrderItemResponseDTO

--- src/main/java/org/majdifoxx/smartshop/dto/response/OrderItemResponseDTO.java
+++ src/main/java/org/majdifoxx/smartshop/dto/response/OrderItemResponseDTO.java
@@ -1,4 +1,21 @@
 package org.majdifoxx.smartshop.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderItemResponseDTO {
+
+    private Long productId;
+    private String productName;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal lineTotal;
 }
