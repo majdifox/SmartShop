2a69dbe296f0c87bca471c5a6648a338a27d0b8a
majdifox <majmehdi20@gmail.com>
2025-11-26 17:00:04 +0100

    creating the product and the user repo

---
+++ src/main/java/org/majdifoxx/smartshop/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package org.majdifoxx.smartshop.repository;
+
+import org.majdifoxx.smartshop.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+
+}
