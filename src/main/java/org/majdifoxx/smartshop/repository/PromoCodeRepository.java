c7d58cabb5a36dbaf8a7653d5bab984205048474
majdifox <majmehdi20@gmail.com>
2025-12-10 02:51:15 +0100

    adding the PromoCode repo

--- src/main/java/org/majdifoxx/smartshop/repository/PromoCodeRepository.java
+++ src/main/java/org/majdifoxx/smartshop/repository/PromoCodeRepository.java
@@ -1,4 +1,11 @@
 package org.majdifoxx.smartshop.repository;
 
-public interface PromoCodeRepository {
+import org.majdifoxx.smartshop.entity.PromoCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PromoCodeRepository extends JpaRepository<PromoCode, Long> {
+    Optional<PromoCode> findByCode(String code);
+    boolean existsByCode(String code);
 }
